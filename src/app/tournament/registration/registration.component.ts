db7e3de3df766d00281e64fc617115a7a6ddad1c
kw00183 <kw00183@cs6261-vm09.uwg.westga.edu>
2021-06-19 03:06:59 -0400

    add registerContestants method to the registration component

--- src/app/tournament/registration/registration.component.ts
+++ src/app/tournament/registration/registration.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Validators, FormBuilder } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { RosterService } from '../../services/roster.service';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -11,22 +13,56 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export class RegistrationComponent {
 
-  public contestantsForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.createForm();
+  public players: Array<string>;
+  public informationMessages: string;
+
+  public contestantsForm: FormGroup = new FormGroup({
+    contestant0: new FormControl(null, [Validators.minLength(2)]),
+    contestant1: new FormControl(null, [Validators.minLength(2)]),
+    contestant2: new FormControl(null, [Validators.minLength(2)]),
+    contestant3: new FormControl(null, [Validators.minLength(2)]),
+    contestant4: new FormControl(null, [Validators.minLength(2)]),
+    contestant5: new FormControl(null, [Validators.minLength(2)]),
+    contestant6: new FormControl(null, [Validators.minLength(2)]),
+    contestant7: new FormControl(null, [Validators.minLength(2)])
+  });
+
+  constructor(rosterService: RosterService) {
+    this.players = ['', '', '', '', '', '', '', ''];
   }
 
-  createForm() {
-    this.contestantsForm = this.fb.group({
-      contestant0: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-      contestant1: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-      contestant2: new FormControl(null, [Validators.minLength(2)]),
-      contestant3: new FormControl(null, [Validators.minLength(2)]),
-      contestant4: new FormControl(null, [Validators.minLength(2)]),
-      contestant5: new FormControl(null, [Validators.minLength(2)]),
-      contestant6: new FormControl(null, [Validators.minLength(2)]),
-      contestant7: new FormControl(null, [Validators.minLength(2)])
-    });
+  registerContestants() {
+    if (rosterService.addContestant(contestant0)
+      && rosterService.addContestant(contestant1)
+      && !rosterService.addContestant(contestant2)) {
+      this.players.push(contestant0);
+      this.players.push(contestant1);
+    } else if (rosterService.addContestant(contestant0)
+      && rosterService.addContestant(contestant1)
+      && rosterService.addContestant(contestant2)
+      && rosterService.addContestant(contestant3)
+      && !rosterService.addContestant(contestant4)) {
+      this.players.push(contestant0);
+      this.players.push(contestant1);
+      this.players.push(contestant2);
+      this.players.push(contestant3);
+    } else if (rosterService.addContestant(contestant0)
+      && rosterService.addContestant(contestant1)
+      && rosterService.addContestant(contestant2)
+      && rosterService.addContestant(contestant3)
+      && rosterService.addContestant(contestant4)
+      && rosterService.addContestant(contestant5)
+      && rosterService.addContestant(contestant6)
+      && rosterService.addContestant(contestant7)) {
+      this.players.push(contestant0);
+      this.players.push(contestant1);
+      this.players.push(contestant2);
+      this.players.push(contestant3);
+      this.players.push(contestant4);
+      this.players.push(contestant5);
+      this.players.push(contestant6);
+      this.players.push(contestant7);
+    }
   }
 
   onSubmit() {
