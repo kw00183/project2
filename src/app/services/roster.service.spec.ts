778fbad7557f4f5079656f3bbeed97a70e91dc17
kw00183 <kw00183@cs6261-vm07.uwg.westga.edu>
2021-06-22 22:58:53 -0400

    add in JUnit tests for the brackets component

--- src/app/services/roster.service.spec.ts
+++ src/app/services/roster.service.spec.ts
@@ -18,42 +18,42 @@ describe('RosterService', () => {
 
   it('should not allow duplicate names', () => {
     const rosterService = new RosterService();
-    rosterService.addContestant('Kim',1);
-    expect(rosterService.addContestant('Kim',1)).toBeFalsy();
+    rosterService.addContestant('Kim',0);
+    expect(rosterService.addContestant('Kim',0)).toBeFalsy();
   });
 
   it('should not allow duplicate names regardless of case', () => {
     const rosterService = new RosterService();
-    rosterService.addContestant('Kim',1);
-    expect(rosterService.addContestant('kim',1)).toBeFalsy();
+    rosterService.addContestant('Kim',0);
+    expect(rosterService.addContestant('kim',0)).toBeFalsy();
   });
 
   it('should not allow null names', () => {
     const rosterService = new RosterService();
-    expect(rosterService.addContestant(null,1)).toBeFalsy();
+    expect(rosterService.addContestant(null,0)).toBeFalsy();
   });
 
   it('should not allow empty string names', () => {
     const rosterService = new RosterService();
-    expect(rosterService.addContestant('',1)).toBeFalsy();
+    expect(rosterService.addContestant('',0)).toBeFalsy();
   });
 
   it('should not allow empty string names of spaces', () => {
     const rosterService = new RosterService();
-    expect(rosterService.addContestant('  ',1)).toBeFalsy();
+    expect(rosterService.addContestant('  ',0)).toBeFalsy();
   });
 
   it('should add one contestant', () => {
     const rosterService = new RosterService();
-    rosterService.addContestant('Jen',1);
+    rosterService.addContestant('Jen',0);
     var array = rosterService.getContestants();
     expect(array.length = 1).toBeTruthy();
   });
 
   it('should add several contestants', () => {
     const rosterService = new RosterService();
-    rosterService.addContestant('Roy',1);
-    rosterService.addContestant('Moss',2);
+    rosterService.addContestant('Roy',0);
+    rosterService.addContestant('Moss',1);
     var array = rosterService.getContestants();
     expect(array.length > 1).toBeTruthy();
   });
--- src/app/tournament/brackets/brackets.component.spec.ts
+++ src/app/tournament/brackets/brackets.component.spec.ts
@@ -1,14 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
 
 import { BracketsComponent } from './brackets.component';
+
+import { RosterService } from '../../services/roster.service';
+import { WinnerService } from '../../services/winner.service';
 
 describe('BracketsComponent', () => {
   let component: BracketsComponent;
   let fixture: ComponentFixture<BracketsComponent>;
+  let rosterService: RosterService;
+  let testRosterService: RosterService;
+  let winnerService: WinnerService;
+  let testWinnerService: WinnerService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BracketsComponent ]
+      imports: [ FormsModule ],
+      declarations: [ BracketsComponent ],
+      providers: [ RosterService, WinnerService ]
     })
     .compileComponents();
   }));
@@ -16,10 +27,77 @@ describe('BracketsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BracketsComponent);
     component = fixture.componentInstance;
+    testWinnerService = TestBed.get(WinnerService);
+    testRosterService = TestBed.get(RosterService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be the same instance of Service injected via inject(...) and TestBed.get(...)', () => {
+    inject([WinnerService], (injectService: WinnerService) => {
+        expect(injectService).toBe(testWinnerService);
+      })
+  });
+
+  it('should show empty brackets on init', () => {
+    expect(component.getBrackets().length).toEqual(0);
+  });
+
+  it('should show 1 item in bracket array when 2 players set', () => {
+    testRosterService.addContestant('Roy',0);
+    testRosterService.addContestant('Bill',1);
+    component.createBrackets();
+    expect(component.getBrackets().length).toEqual(1);
+  });
+
+  it('should show 2 items in bracket array when 4 players set', () => {
+    testRosterService.addContestant('Roy',0);
+    testRosterService.addContestant('Bill',1);
+    testRosterService.addContestant('Ted',2);
+    testRosterService.addContestant('Percy',3);
+    component.createBrackets();
+    expect(component.getBrackets().length).toEqual(2);
+  });
+
+  it('should show 4 items in bracket array when 8 players set', () => {
+    testRosterService.addContestant('Roy',0);
+    testRosterService.addContestant('Bill',1);
+    testRosterService.addContestant('Ted',2);
+    testRosterService.addContestant('Percy',3);
+    testRosterService.addContestant('Tom',4);
+    testRosterService.addContestant('Jaxon',5);
+    testRosterService.addContestant('Jules',6);
+    testRosterService.addContestant('Jos',7);
+    component.createBrackets();
+    expect(component.getBrackets().length).toEqual(4);
+  });
+
+  it('should produce a winners list from matches with a winner', () => {
+    let match1 = ['Roy','Dan','Dan'];
+    let match2 = ['Rick','Morty','Rick'];
+    let winners = [match1,match2]
+    component.selectWinners(winners);
+    expect(testWinnerService.getWinners().length).toEqual(2);
+  });
+
+  it('should not produce a winners list from matches with a missing winner', () => {
+    let match1 = ['Roy','Dan',''];
+    let match2 = ['Rick','Morty','Rick'];
+    let winners = [match1,match2]
+    component.selectWinners(winners);
+    expect(testWinnerService.getWinners().length).toEqual(0);
+  });
+
+  it('should reset observable contestants when resetObservableContestants is called after a final winner is chosen', () => {
+    testRosterService.addContestant('Roy',0);
+    testRosterService.addContestant('Bill',1);
+    testRosterService.addContestant('Ted',2);
+    testRosterService.addContestant('Percy',3);
+    expect(testRosterService.getContestants().length).toEqual(4);
+    component.resetObservableContestants();
+    expect(testRosterService.getContestants().length).toEqual(0);
   });
 });
